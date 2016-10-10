009ffe1c6b976d582376c0a7988639141db0815a
Nathan McElwain <nathanmcelwain@ymail.com>
2016-10-10 15:12:49 -0700

    refixing same thing again

--- _build/js/main.js
+++ _build/js/main.js
@@ -6,8 +6,14 @@ $(document).ready(function(){
   window.onload = googleClient;
 
   $('#searcher').on('mousedown', function(){
-    getResults();
+    if($('input[type=text]').val() === ''){
+      $('#search-results').append("<h2>Wait! I need an artist to search for!</h2>")
+    }
+    else{
+      getResults();
+    }
   });
+  
   $(document).keypress(function(e){
     if(e.which === 13){
       if($('input[type=text]').val() === ''){
